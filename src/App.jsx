532a0db8386941ae59080866c6aaf9096664ec46
Uriel Martinez <martinezuriel651@gmail.com>
2023-12-07 14:11:21 -0600

    Rendered header in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Header />
     </div>
   );
 }
 
 export default App;
-
