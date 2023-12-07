d9bd0ea06b371c7c40070c476db2051f6f4c0f5c
Uriel Martinez <martinezuriel651@gmail.com>
2023-12-07 14:59:08 -0600

    Addition of Movie Object

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import MovieList from "./components/MovieList/MovieList";
 import MovieInfo from "./components/MovieInfo/MovieInfo";
 
 function App() {
+  const selectedMovie = {
+    title: "Forrest Gump",
+    runningTime: 142,
+    genre: "Romantic Drama",
+  };
+
   return (
     <div className="App">
       <Header />
       <MovieList />
-      <MovieInfo
-        title="Forrest Gump"
-        runningTime={142}
-        genre="Romantic Drama"
-      />
+      <MovieInfo movieObj={selectedMovie} />
     </div>
   );
 }
--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,15 +1,15 @@
-const MovieInfo = ({ title, runningTime, genre }) => {
+const MovieInfo = ({ movieObj }) => {
   return (
     <div>
-      <h4>{title}</h4>
+      <h4>{movieObj.title}</h4>
       <div>
         <div>
           <span>Running Time</span>
-          <span>{runningTime}</span>
+          <span>{movieObj.runningTime}</span>
         </div>
         <div>
           <span>Genre</span>
-          <span>{genre}</span>
+          <span>{movieObj.genre}</span>
         </div>
       </div>
     </div>
