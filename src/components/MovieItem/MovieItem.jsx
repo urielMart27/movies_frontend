68c0ba30d7c80ffb6a1ae2e02c35f9e8c2b62071
Uriel Martinez <martinezuriel651@gmail.com>
2023-12-08 13:11:39 -0600

    Start of MovieItem CSS

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,11 +1,21 @@
+import React, { useState } from "react";
+import "./MovieItem.css";
+
 const MovieItem = ({ title }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  console.log(isFavorite);
   const handleClick = (e) => {
-    console.log(e);
+    setIsFavorite(!isFavorite);
   };
+
+  const btnClass = isFavorite ? "active-btn" : "";
   return (
     <div className="movie-item">
       <span>{title}</span>
-      <button onClick={handleClick}>Favorite</button>
+      <button className={btnClass} onClick={handleClick}>
+        Favorite
+      </button>
     </div>
   );
 };
