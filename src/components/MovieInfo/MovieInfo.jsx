bd05a38530fd59b04ddd7fb718e7ecfaa46c4195
Uriel Martinez <martinezuriel651@gmail.com>
2023-12-14 14:07:46 -0600

    Added Image to MovieInfo

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,10 +1,35 @@
 import "./MovieInfo.css";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 const MovieInfo = ({ movieObj }) => {
+  const [flixData, setFlixData] = useState(null);
+
+  console.log("flixData:", flixData);
+
+  const fetchFlixData = async () => {
+    try {
+      const response = await axios.get(
+        `https://flixdb.glitch.me/api/movies?key=3Hd9zT4wQ6LmB7pY2jK1Xr8V&title=${movieObj.title}`
+      );
+      setFlixData(response.data);
+    } catch (error) {
+      console.warn("Error fetching flix data in MovieInfo: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFlixData();
+  }, [movieObj]);
+
   return (
     movieObj && (
       <div className="flex-item">
         <h4>{movieObj.title}</h4>
+        <div className="poster-wrapper">
+          <img src={flixData?.poster_url} />
+        </div>
         <div>
           <div className="data-item">
             <span>Running Time</span>
@@ -13,6 +38,10 @@ const MovieInfo = ({ movieObj }) => {
           <div className="data-item">
             <span>Genre</span>
             <span>{movieObj.genre}</span>
+          </div>
+          <div className="data-item">
+            <span>Release Date</span>
+            <span>{flixData?.release_date}</span>
           </div>
         </div>
       </div>
